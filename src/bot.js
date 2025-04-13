7abbe2315e7b66cdeddf22c74f86e5b3db5421a6
Max Dragaytsev <dragaytsev@air.agency>
2025-04-13 12:48:23 +0300

    upd

--- src/bot.js
+++ src/bot.js
@@ -1,93 +1,27 @@
 const {Telegraf} = require('telegraf');
 const axios = require('axios')
+const dotenv = require('dotenv')
+const {messageHandler, commandClearHandler, imageCommandHandler} = require('./handlers')
 
-require('dotenv').config()
+dotenv.config()
+const tgBotToken = process.env.DMAX1447_BOT_TOKEN
 
-
-const bot = new Telegraf(process.env.DMAX1447_BOT_TOKEN);
-let messages = []
-let timerId = null
-
-bot.on('message', async (ctx) => {
-  console.log(ctx.message)
-  if (ctx.message.text.includes('dmax1447_bot')) {
-    const messageContent = ctx.message.text.replace('@dmax1447_bot', '')
-    if (timerId) {
-      clearTimeout(timerId)
+function initBot(botToken) {
+  console.log('starting bot')
+  const bot = new Telegraf(botToken);
+  bot.telegram.setMyCommands([
+    {
+      command: 'clear',
+      description: 'Clear cache ',
     }
-    timerId = setTimeout(() => {
-      messages = []
-    }, 1000 * 60 * 5)
+  ]);
+  bot.command('clear', commandClearHandler)
+  bot.command('image', imageCommandHandler)
+  bot.on('message', messageHandler)
+  bot.launch()
+}
 
-    if (messageContent.includes('/clear')) {
-      messages = []
-      ctx.reply('история сообщений очищена')
-      return
-    }
-
-    if (messageContent.includes('картинка')) {
-      console.log('try generate image from', messageContent)
-      try {
-        const result = await axios.post(
-          'https://api.openai.com/v1/images/generations',
-          {
-            "n": 1,
-            "size": "512x512",
-            "prompt": messageContent
-          },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${process.env.CHAT_TOKEN}`
-            },
-          }
-        ).then(response => response.data.data[0])
-        console.log('result:')
-        console.log(result.url)
-        ctx.reply(result.url)
-      } catch (e) {
-        console.log(e.message)
-        ctx.reply(Math.random() > 0.4 ? 'Устал. Убить всех человеков' : 'Bite my shiny metal ass')
-      }
-    } else {
-      try {
-        const userMessage = {
-          role: 'user', content: messageContent
-        }
-        if (messages.length > 8) {
-          messages.shift()
-        }
-        messages.push(userMessage)
-        const result = await axios.post(
-          'https://api.openai.com/v1/chat/completions',
-          {
-            "model": "gpt-3.5-turbo",
-            max_tokens: 512,
-            messages
-          },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${process.env.CHAT_TOKEN}`
-            },
-          }
-        ).then(responce => responce.data)
-        const systemMessage = result.choices[0].message
-        if (messages.length > 8) {
-          messages.shift()
-        }
-        messages.push(systemMessage)
-        ctx.reply(systemMessage.content)
-      } catch (e) {
-        console.log(e.message)
-        ctx.reply(Math.random() > 0.4 ? 'Устал. Убить всех человеков' : 'Bite my shiny metal ass')
-      }
-    }
-  }
-})
-
-bot.hears('hi', (ctx) => ctx.reply('white power!'));
-bot.launch();
+initBot(tgBotToken)
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
---
+++ src/handlers.js
@@ -0,0 +1,98 @@
+const axios = require("axios");
+let messages = []
+let timerId = null
+const dotenv = require('dotenv')
+
+dotenv.config()
+const chatGPTToken = process.env.CHAT_TOKEN_KV
+
+function imageCommandHandler(ctx) {
+  const prompt = ctx.update.message.text.replace('/image', '')
+  return axios.post(
+    'https://api.openai.com/v1/images/generations',
+    {
+      model: "dall-e-3",
+      prompt,
+      n: 1,
+      size: "1024x1024",
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${chatGPTToken}`
+      },
+    }
+  )
+    .then(response => {
+      const result = response.data.data[0]
+      ctx.replyWithPhoto(result.url)
+    })
+    .catch(err => {
+      ctx.reply(err.message)
+    })
+}
+
+async function messageHandler(ctx) {
+  const replyUser = ctx.message.reply_to_message && ctx.message.reply_to_message.from.username
+  const messageText = ctx.message.text || ''
+  const shouldReply = messageText.includes('dmax1447_bot') || replyUser === 'dmax1447_bot'
+  // console.log('raw message:', ctx.message)
+  // console.log({replyUser, messageText, shouldReply})
+
+  if (!shouldReply) {
+    return
+  }
+
+  const messageContent = ctx.message.text.replace('@dmax1447_bot', '')
+  if (timerId) {
+    clearTimeout(timerId)
+  }
+  timerId = setTimeout(() => {
+    messages = []
+  }, 1000 * 60 * 5)
+
+  try {
+    const userMessage = {
+      role: 'user', content: messageContent
+    }
+    if (messages.length > 8) {
+      messages.shift()
+    }
+    messages.push(userMessage)
+    const result = await axios.post(
+      'https://api.openai.com/v1/chat/completions',
+      {
+        model: "gpt-4o",
+        max_tokens: 512,
+        messages
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${chatGPTToken}`
+        },
+      }
+    ).then(response => response.data)
+    const systemMessage = result.choices[0].message
+    if (messages.length > 8) {
+      messages.shift()
+    }
+    messages.push(systemMessage)
+    ctx.reply(systemMessage.content)
+  } catch (e) {
+    console.log(e.message)
+    ctx.reply(`Ошибка генерации ответа: ${e.message}`)
+  }
+}
+
+function commandClearHandler(ctx) {
+  console.log('clearHandler')
+  messages = []
+  ctx.reply('история сообщений очищена')
+  return
+}
+
+module.exports = {
+  messageHandler,
+  commandClearHandler,
+  imageCommandHandler
+}
